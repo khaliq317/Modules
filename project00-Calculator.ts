216e9f13372890f8fb0cd25157b5d3a3259d1595
Khaliq Abbas <140581171+khaliq317@users.noreply.github.com>
2023-09-01 10:51:15 +0500

    first commit

---
+++ project00-Calculator.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let input= await inquirer.prompt([
+    {
+        name:"Num1",
+        type:"number",
+        message:"Please enter your first Number",
+
+    },
+    {
+        name:"Num2",
+        type:"number",
+        message:"Please enter your second Number",
+    },
+    {
+        name:"Operation",
+        type:"list",
+        choices:["Addition","Subtraction","Multiplication","Division"],
+        message:"Select Operator",
+    }]);
+
+    if (input.Operation==="Addition"){
+        console.log(`Your answer is: ${input.Num1+input.Num2}`); 
+    } else if (input.Operation==="Subtraction"){
+        console.log(`Your answer is: ${input.Num1-input.Num2}`);
+    } else if (input.Operation==="Multiplication"){
+        console.log(`Your answer is: ${input.Num1*input.Num2}`);
+    } else if (input.Operation==="Division"){
+        console.log(`Your answer is: ${input.Num1/input.Num2}`);
+    } else{
+        console.log("Inavalid Input");
+    }
